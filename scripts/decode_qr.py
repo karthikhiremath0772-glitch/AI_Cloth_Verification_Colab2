4bfb2b8a0b7e3eab15440cc9a787063db223bf52
karthikhiremath0772-glitch <karthikhiremath0772@gmail.com>
2025-10-07 00:08:01 +0530

    Update decode_qr.py

--- scripts/decode_qr.py
+++ scripts/decode_qr.py
@@ -1,10 +1,10 @@
-import qrcode
-from io import BytesIO
+# import qrcode
+# from io import BytesIO
 
-def generate_qr_for_product(features):
-    features_str = ','.join([str(f) for f in features])
-    qr = qrcode.QRCode(version=1, box_size=10, border=4)
-    qr.add_data(features_str)
-    qr.make(fit=True)
-    img = qr.make_image(fill='black', back_color='white')
-    return img, features_str
+# def generate_qr_for_product(features):
+#     features_str = ','.join([str(f) for f in features])
+#     qr = qrcode.QRCode(version=1, box_size=10, border=4)
+#     qr.add_data(features_str)
+#     qr.make(fit=True)
+#     img = qr.make_image(fill='black', back_color='white')
+#     return img, features_str
